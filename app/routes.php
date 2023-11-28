700f03871aa18f0997252c738a31058a2487c702
Justin-vdm <126683510+Justin-vdm@users.noreply.github.com>
2023-11-28 20:06:31 +0100

    Revert "Init"
    This reverts commit 00791d16935ed993479b08247736f6cc5de1e6d6.

--- app/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Foundation\Support\Facades\Router;
-
-Router::get('/welkom', static function () {
-    print("working");
-    exit;
-});
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const BASE_PATH = __DIR__.'/../';
-
-require BASE_PATH.'vendor/autoload.php';
-
-require_once BASE_PATH.'app/Foundation/helpers.php';
-require_once BASE_PATH.'bootstrap.php';
