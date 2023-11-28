3703b5e20f587e02f9e8a59108f3249c3a416fec
Justin-vdm <126683510+Justin-vdm@users.noreply.github.com>
2023-11-28 22:12:12 +0100

    Init part 1

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <?php include_once __DIR__ . "/../application/components/layout/head.phtml"; ?>
+
+    <body>
+        <?php include_once __DIR__ . "/../application/components/layout/header.phtml"; ?>
+
+        <div>
+
+        </div>
+
+        <?php include_once __DIR__ . "/../application/components/layout/header.phtml"; ?>
+    </body>
+</html>
