da25cc8b33a9a7cf0314d4a5d936ee7417df9aa1
Justin-vdm <126683510+Justin-vdm@users.noreply.github.com>
2023-12-03 13:45:36 +0100

    Opzet template aanpassingen

--- application/components/layout/footer.php
+++ application/components/layout/footer.php
@@ -1,3 +1,5 @@
-<footer class="container-fluid h-fit px-4 py-3 row gx-0 items-center shadow-sm bg-light">
-    <p class="m-0 col-12 d-flex justify-content-center text-muted">&copy; The Sixth String</p>
+<footer class="container-fluid h-fit d-flex bg-light">
+    <div class="container-lg mx-auto py-3">
+        <p class="m-0 col-12 d-flex justify-content-center text-muted">&copy; The Sixth String</p>
+    </div>
 </footer>
--- application/components/layout/header.php
+++ application/components/layout/header.php
@@ -1,30 +1,37 @@
-<header class="container-fluid h-fit px-4 py-3 row gx-0 items-center shadow-sm">
-    <div class="col d-flex align-items-center">
-        <p class="m-0 fw-bold text-dark">
-            The Sixt String
-        </p>
-    </div>
-
-    <div class="col-6 d-flex justify-content-center">
-        <div class="input-group">
-            <input type="text" name="search" id="header-search"
-                   placeholder="Product zoeken."
-                   aria-label="Product zoeken."
-                   aria-describedby="addon-wrapping"
-                   class="form-control border"
-            >
-            <button class="btn btn-outline-light border text-dark" type="button">
-                <i class="fa fa-search"></i>
-            </button>
+<header class="container-fluid h-fit d-flex justify-content-center shadow-sm bg-white">
+    <div class="container-lg mx-auto py-3 row gx-0 items-center">
+        <!--Logo-->
+        <div class="col d-flex align-items-center">
+            <a href="/">
+                <p class="m-0 fw-bold text-dark">
+                    The Sixt String
+                </p>
+            </a>
         </div>
-    </div>
 
-    <div class="col d-flex align-items-center justify-content-end">
-        <a href="/profile" class="btn btn-link me-2">
-            <i class="fa fa-user fa-lg text-dark"></i>
-        </a>
-        <a href="/profile" class="btn btn-link">
-            <i class="fa fa-cart-shopping fa-lg text-dark"></i>
-        </a>
+        <!--Zoekbalk-->
+        <div class="col-6 d-flex justify-content-center">
+            <div class="input-group">
+                <input type="text" name="search" id="header-search"
+                       placeholder="Product zoeken."
+                       aria-label="Product zoeken."
+                       aria-describedby="addon-wrapping"
+                       class="form-control border"
+                >
+                <button class="btn btn-outline-light border text-dark" type="button">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <!--Knoppen rechts-->
+        <div class="col d-flex align-items-center justify-content-end">
+            <a href="/profiel" class="btn btn-link me-2">
+                <i class="fa fa-user fa-lg text-dark"></i>
+            </a>
+            <a href="/winkelwagen" class="btn btn-link">
+                <i class="fa fa-cart-shopping fa-lg text-dark"></i>
+            </a>
+        </div>
     </div>
 </header>
