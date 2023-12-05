8135886df1ee09c481e72f92f21c63815aa9bc7e
Huijgen-IT <146438593+Huijgen-IT@users.noreply.github.com>
2023-12-05 19:49:06 +0100

    producten pagina toegevoegd

--- public/producten.php
+++ public/producten.php
@@ -4,20 +4,33 @@
 <!DOCTYPE html>
 
 <html lang="en">
-    <!--Head-->
-    <?php include __DIR__ . "/../application/components/layout/head.php"; ?>
+<!--Head-->
+<?php include __DIR__ . "/../application/components/layout/head.php"; ?>
 
-    <body class="min-vw-100 min-vh-100 d-flex flex-column bg-white">
-        <!--Header-->
-        <?php include __DIR__ . "/../application/components/layout/header.php"; ?>
+<body class="min-vw-100 min-vh-100 d-flex flex-column bg-white">
+<!--Header-->
+<?php include __DIR__ . "/../application/components/layout/header.php"; ?>
 
-        <!--Pagina content container-->
-        <div class="container-lg flex-grow-1 gx-0 py-4">
-            <!--Plaats hier de pagina elementen-->
+<!--Pagina content container-->
+<div class="container-lg flex-grow-1 gx-0 py-4">
+    <div class="d-flex justify-content-center">
+        <h1 class="mt-0 font-weight-bold mb-5">Producten</h1>
+    </div>
+    <!--Weergave categorien-->
+    <div class="container-lg flex-grow-1 gx-o py-4">
+        <div class="list-group justify-content-start" style="max-width: 25%">
+            <h5 class="font-weight-bold d-flex justify-content-center">Categorien</h5>
+            <a href="#" class="list-group-item list-group-item-action">Akoestische gitaar</a>
+            <a href="#" class="list-group-item list-group-item-action">Electrische gitaar</a>
+            <a href="#" class="list-group-item list-group-item-action">Basgitaar</a>
+            <a href="#" class="list-group-item list-group-item-action">Kindergitaar</a>
         </div>
+    </div>
+</div>
+<!--Plaats hier de pagina elementen-->
 
-        <!--Footer & Scripts-->
-        <?php include __DIR__ . "/../application/components/layout/footer.php"; ?>
-        <?php include __DIR__ . "/../application/components/layout/scripts.php"; ?>
-    </body>
+<!--Footer & Scripts-->
+<?php include __DIR__ . "/../application/components/layout/footer.php"; ?>
+<?php include __DIR__ . "/../application/components/layout/scripts.php"; ?>
+</body>
 </html>
