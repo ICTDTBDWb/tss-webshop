4717971a5b5d0dd82a298adc0991173961d7690c
Huijgen-IT <146438593+Huijgen-IT@users.noreply.github.com>
2023-12-08 09:38:04 +0100

    Producten pagina toegevoegd

--- application/producten.php
+++ application/producten.php
@@ -1,1 +1,74 @@
 <?php
+
+include __DIR__ . '/DatabaseManager.php';
+
+/**
+ * Haalt meerdere items op uit de database.
+ */
+function queryCategorieen() {
+    $database = new \application\DatabaseManager(); // Maak een instantie van de DatabaseManager klasse.
+    $result = $database->query("SELECT naam FROM categorieen")->get(); // Voer een query uit en haal meerdere rijen op.
+
+    $database->close(); // Sluit de database connectie.
+
+    return $result;
+}
+
+/**
+ * Haalt een enkel item op uit de database op basis van het opgegeven ID.
+ */
+function queryCategorie1() {
+    $database = new \application\DatabaseManager(); // Maak een instantie van de DatabaseManager klasse.
+    $result = $database->query(
+        "SELECT naam FROM categorieen WHERE id = ?",
+        [1] // Haal de rij op met het opgegeven ID (bijv. ID = 1).
+    )->first(); // Voer een query uit en haal een enkele rij op.
+
+    $database->close(); // Sluit de database connectie.
+
+    return $result;
+}
+
+function queryCategorie2() {
+    $database = new \application\DatabaseManager(); // Maak een instantie van de DatabaseManager klasse.
+    $result = $database->query(
+        "SELECT naam FROM categorieen WHERE id = ?",
+        [2] // Haal de rij op met het opgegeven ID (bijv. ID = 1).
+    )->first(); // Voer een query uit en haal een enkele rij op.
+
+    $database->close(); // Sluit de database connectie.
+
+    return $result;
+}
+
+function queryCategorie3() {
+    $database = new \application\DatabaseManager(); // Maak een instantie van de DatabaseManager klasse.
+    $result = $database->query(
+        "SELECT naam FROM categorieen WHERE id = ?",
+        [4] // Haal de rij op met het opgegeven ID (bijv. ID = 1).
+    )->first(); // Voer een query uit en haal een enkele rij op.
+
+    $database->close(); // Sluit de database connectie.
+
+    return $result;
+}
+
+function queryCategorie4() {
+    $database = new \application\DatabaseManager(); // Maak een instantie van de DatabaseManager klasse.
+    $result = $database->query(
+        "SELECT naam FROM categorieen WHERE id = ?",
+        [3] // Haal de rij op met het opgegeven ID (bijv. ID = 1).
+    )->first(); // Voer een query uit en haal een enkele rij op.
+
+    $database->close(); // Sluit de database connectie.
+
+    return $result;
+}
+
+$categorie1 = queryCategorie1();
+$categorie2 = queryCategorie2();
+$categorie3 = queryCategorie3();
+$categorie4 = queryCategorie4();
+
+
+?>
--- public/producten.php
+++ public/producten.php
@@ -12,18 +12,87 @@
 <?php include __DIR__ . "/../application/components/layout/header.php"; ?>
 
 <!--Pagina content container-->
-<div class="container-lg flex-grow-1 gx-0 py-4">
-    <div class="d-flex justify-content-center">
-        <h1 class="mt-0 font-weight-bold mb-5">Producten</h1>
+    <div class="container-lg flex-grow-1 gx-0 py-4">
+        <div class="d-flex justify-content-center">
+            <h1 class="mt-0 font-weight-bold mb-5">Producten</h1>
+        </div>
     </div>
-    <!--Weergave categorien-->
+        <!--Weergave categorien-->
     <div class="container-lg flex-grow-1 gx-o py-4">
-        <div class="list-group justify-content-start" style="max-width: 25%">
-            <h5 class="font-weight-bold d-flex justify-content-center">Categorien</h5>
-            <a href="#" class="list-group-item list-group-item-action">Akoestische gitaar</a>
-            <a href="#" class="list-group-item list-group-item-action">Electrische gitaar</a>
-            <a href="#" class="list-group-item list-group-item-action">Basgitaar</a>
-            <a href="#" class="list-group-item list-group-item-action">Kindergitaar</a>
+        <div class="row">
+            <div class="col-sm-2">
+                <h5 class="font-weight-bold d-flex justify-content-center" >Categorien</h5>
+                <a href="#" class="list-group-item list-group-item-action" <?php print "<pre>". $categorie1['naam'] ."</pre>"; ?></a>
+                <a href="#" class="list-group-item list-group-item-action" <?php print "<pre>". $categorie2['naam'] ."</pre>"; ?></a>
+                <a href="#" class="list-group-item list-group-item-action" <?php print "<pre>". $categorie3['naam'] ."</pre>"; ?></a>
+                <a href="#" class="list-group-item list-group-item-action" <?php print "<pre>". $categorie4['naam'] ."</pre>"; ?></a>
+            </div>
+            <div class="col-md">
+                <h5 href="#" class="font-weight-bold d-flex justify-content-center">Product 1</h5>
+                <img
+                    src="https://img.freepik.com/free-photo/guy-playing-acoustic-guitar_169016-2126.jpg?w=1380&t=st=1701540133~exp=1701540733~hmac=7522639adac31beacdcfcb3c31df1aeb3666b4d6547be7ad761153305b7025f8"
+                    alt="Banner"
+                    class="rounded w-100 h-100"
+                    style="object-fit: cover"
+                >
+                <a class="d-flex justify-content-center">Productomschrijving</a>
+            </div>
+            <div class="col-md">
+                <h5 href="#" class="font-weight-bold d-flex justify-content-center">Product 2</h5>
+                <img
+                    src="https://img.freepik.com/free-photo/guy-playing-acoustic-guitar_169016-2126.jpg?w=1380&t=st=1701540133~exp=1701540733~hmac=7522639adac31beacdcfcb3c31df1aeb3666b4d6547be7ad761153305b7025f8"
+                    alt="Banner"
+                    class="rounded w-100 h-100"
+                    style="object-fit: cover"
+                >
+                <a class="d-flex justify-content-center">Productomschrijving</a>
+            </div>
+            <div class="col-md">
+                <h5 href="#" class="font-weight-bold d-flex justify-content-center">Product 3</h5>
+                <img
+                    src="https://img.freepik.com/free-photo/guy-playing-acoustic-guitar_169016-2126.jpg?w=1380&t=st=1701540133~exp=1701540733~hmac=7522639adac31beacdcfcb3c31df1aeb3666b4d6547be7ad761153305b7025f8"
+                    alt="Banner"
+                    class="rounded w-100 h-100"
+                    style="object-fit: cover"
+                >
+                <a class="d-flex justify-content-center">Productomschrijving</a>
+            </div>
+        </div>
+    </div>
+    <div class="container-lg flex-grow-1 gx-o py-4">
+        <div class="row">
+            <div class="col-sm-2">
+            </div>
+            <div class="col-md">
+                <h5 href="#" class="font-weight-bold d-flex justify-content-center">Product 4</h5>
+                <img
+                        src="https://img.freepik.com/free-photo/guy-playing-acoustic-guitar_169016-2126.jpg?w=1380&t=st=1701540133~exp=1701540733~hmac=7522639adac31beacdcfcb3c31df1aeb3666b4d6547be7ad761153305b7025f8"
+                        alt="Banner"
+                        class="rounded w-100 h-100"
+                        style="object-fit: cover"
+                >
+                <a class="d-flex justify-content-center">Productomschrijving</a>
+            </div>
+            <div class="col-md">
+                <h5 href="#" class="font-weight-bold d-flex justify-content-center">Product 5</h5>
+                <img
+                        src="https://img.freepik.com/free-photo/guy-playing-acoustic-guitar_169016-2126.jpg?w=1380&t=st=1701540133~exp=1701540733~hmac=7522639adac31beacdcfcb3c31df1aeb3666b4d6547be7ad761153305b7025f8"
+                        alt="Banner"
+                        class="rounded w-100 h-100"
+                        style="object-fit: cover"
+                >
+                <a class="d-flex justify-content-center">Productomschrijving</a>
+            </div>
+            <div class="col-md">
+                <h5 href="#" class="font-weight-bold d-flex justify-content-center">Product 6</h5>
+                <img
+                        src="https://img.freepik.com/free-photo/guy-playing-acoustic-guitar_169016-2126.jpg?w=1380&t=st=1701540133~exp=1701540733~hmac=7522639adac31beacdcfcb3c31df1aeb3666b4d6547be7ad761153305b7025f8"
+                        alt="Banner"
+                        class="rounded w-100 h-100"
+                        style="object-fit: cover"
+                >
+                <a class="d-flex justify-content-center">Productomschrijving</a>
+            </div>
         </div>
     </div>
 </div>
