3a4a72b325c0dc784e7014a8dfe68239d3e68594
Huijgen-IT <146438593+Huijgen-IT@users.noreply.github.com>
2023-12-08 10:35:42 +0100

    Beheermenu toegevoegd

--- public/beheer/klanten.php
+++ public/beheer/klanten.php
@@ -11,22 +11,13 @@
 <!--Header-->
 <?php include __DIR__ . "/../../application/components/layout/header.php"; ?>
 
+<!--Menu-->
+<?php include __DIR__ . "/../../application/beheer/menu.php"; ?>
+
 <!--Pagina content container-->
-<div class="container-lg flex-grow-1 gx-0 py-4">
-    <div class="d-flex justify-content-center">
-        <h1 class="mt-0 font-weight-bold mb-5">Beheerdersportaal</h1>
-    </div>
-    <!--Button overzicht-->
-    <div class="mt-2 mb-5 d-flex justify-content-evenly" style="max-width: 75%">
-    <a class="btn btn-secondary" href="#" role="button">Beheeroverzicht</a>
-    <a class="btn btn-secondary" href="#" role="button">Accountgegevens</a>
-    <a class="btn btn-secondary" href="#" role="button">Productbeheer</a>
-    <a class="btn btn-secondary" href="#" role="button">Overzicht bestellingen</a>
-    <a class="btn btn-secondary active" href="/beheer/klanten.php" role="button">Klantbeheer</a>
-    </div>
-            <!--Zoekbalk rechts-->
-            <div class="input-group d-flex justify-content-end">
-            <input type="text" name="search" id="header-search" style="max-width: 25%"
+<!--Zoekbalk rechts-->
+<div class="input-group d-flex justify-content-end">
+    <input type="text" name="search" id="header-search" style="max-width: 25%"
                    placeholder="Klant zoeken."
                    aria-label="Klant zoeken."
                    aria-describedby="addon-wrapping"
