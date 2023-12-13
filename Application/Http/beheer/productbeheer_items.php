b07fca538b94383bdcbfe16f3d783f188aa307c1
juulius <julianvisscher@hotmail.com>
2023-12-13 22:09:09 +0100

    test

--- Application/Http/beheer/productbeheer_items.php
+++ Application/Http/beheer/productbeheer_items.php
@@ -17,171 +17,69 @@
 
 </style>
 
-<div class="accordion" id="accordionPanelsStayOpenExample">
-
-    <?php echo get_arcordion_items() ?>
-
-</div>
 
 
 <?php
 
-//include __DIR__ . "/../../Application/DatabaseManager.php";
 
-use \application\DatabaseManager;
+function arcordion_constructer($categorieen)
+{
+
+    $contruct = "";
+    foreach ($categorieen as $key => $categorie) {
+
+        $show_categorie = $categorie['show'];
 
 
-$filter = "%%";
-if (is_array($_POST) && !empty($_POST)) {
+        $contruct .= "<div class='accordion-item'>
+                            <h2 class='accordion-header' id='accordion-header_$key'>
+                                <button class='accordion-button $show_categorie ' type='button' data-bs-toggle='collapse' data-bs-target='#accordion-collapse_$key' aria-expanded='false' aria-controls='accordion-collapse_$key' >
+                                    $key
+                                </button>
+                            </h2>"
+                                . arcordion_item_constructor($categorie, $key)." 
+                        </div>";
+    }
 
-    var_dump($_POST);
-    //echo ("<script>alert('$test');</script>");
+    return $contruct;
 }
 
 
-
-
-function arcordion_constructer($categorieen)
+    function arcordion_item_constructor($categorie, $key)
     {
-
-
-        $show_categorie = "collapsed";
-        $show_product = "collapse";
-        if (is_array($_GET) && !empty($_GET['Product'])) {
-            $product = filter_input(INPUT_GET, 'Product', FILTER_SANITIZE_SPECIAL_CHARS);
-            $product = preg_split("/ /", $product);
-            var_dump($product);
-        }
-
-        $contruct = "";
-        foreach ($categorieen as $categorie) {
-
-            if (!empty($product[1]) && $categorie['id'] == $product[1])
-            {
-                $show_categorie = "collapsed";
-                $show_product = "collapse";
-            }
-            else
-            {
-                $show_categorie = "collapsed";
-                $show_product = "collapse";
-            }
-
-
-            $contruct .= "<div class='accordion-item'>
-                            <h2 class='accordion-header' id=".encode("accordion-header".$categorie['id']).">
-                                <button class='accordion-button $show_categorie ' type='button' data-bs-toggle='collapse' data-bs-target=" . encode("#accordion-collapse" . $categorie['id']) . " aria-expanded='false' aria-controls=".encode("accordion-collapse" . $categorie['id']). ">"
-                                    . $categorie['naam'] .
-                                "</button>
-                            </h2>"
-                            . arcordion_item_constructor($categorie,false,  $show_product)." 
-                        </div>";
-        }
-
-        $producten = get_overige_producten();
-
-            if (!empty($product[1]) && "overig" == strtolower($product[1]))
-            {
-                $show_categorie = "";
-                $show_product = "collapse show";
-            }
-            else
-            {
-                $show_categorie = "collapsed";
-                $show_product = "collapse";
-            }
-
-
-            $contruct .= "<div class='accordion-item'>
-                            <h2 class='accordion-header' id=".encode("accordion-headeroverig").">
-                                <button class='accordion-button $show_categorie' type='button' data-bs-toggle='collapse' data-bs-target='#accordion-collapseoverig' aria-expanded='false' aria-controls='accordion-collapseoverig'>"
-                                    . "Overig" .
-                                "</button>                          
-                            </h2>"
-                                . arcordion_item_constructor($producten, true,  $show_product)." 
-                         </div>";
-
-
-
-
-        return $contruct;
-    }
-
-    function arcordion_item_constructor($categorie,$overig,$show)
-    {
-        if (!$overig) {
-            $ID = $categorie['id'];
-            $producten = get_producten($categorie['id']);
-        }
-        else {
-            $ID = "overig";
-            $producten = $categorie;
-        }
-
-
-        $contruct = "<div id=".encode("accordion-collapse".$ID)."class='accordion-collapse $show ' aria-labelledby=".encode("accordion-header".$ID).">
+        $show_categorie = $categorie['show'];
+        $contruct = "<div id='accordion-collapse_$key' class='accordion-collapse $show_categorie ' aria-labelledby='accordion-header_$key' >
                             <div class='accordion-body'>";
 
-        if(empty($producten))
-        {
-            $contruct .= "<strong></strong>";
-        }
 
-        foreach($producten as $product)
-        {
-            $contruct .= "
-                            <button class='btn btn-light' style='width: 100%; margin-top: 2%' id=".encode("btn_product_".$product['id'])." name='Product' value=".encode($product['id']." ".$ID).">
-                                <strong>".$product['naam']."</strong>
+        foreach ($categorie as $_key => $product) {
+
+                if(is_array($product)) {
+                    $product_naam = $product['naam'];
+                    $product_id = $product['id'];
+                    $contruct .= "
+                            <button class='btn btn-light' style='width: 100%; margin-top: 2%' id='btn_product_$product_id' name='Product' value='$product_id $key'>
+                                <strong>$product_naam</strong>
                             </button>
                           ";
-
+                }
+                else
+                    $contruct .= "<strong></strong>";
 
         }
 
-        $contruct .= "</div>
+
+
+            $contruct .= "</div>
                      </div>";
+
 
         return $contruct;
     }
 
-    function get_arcordion_items()
-    {
-        $categorieen = get_categorieen();
-        return arcordion_constructer($categorieen);
-    }
-
-    function get_categorieen()
-    {
-        $database = new DatabaseManager();
-        $categorieen = $database->query("SELECT * FROM categorieen ORDER BY id ASC")->get();
-        $database->close();
-        return $categorieen;
-    }
 
 
-    function get_producten($categorie_id)
-    {
 
-        if (is_array($_GET) && !empty($_GET))
-            $filter = filter_input(INPUT_GET,'filter', FILTER_SANITIZE_SPECIAL_CHARS );
 
-        $database = new DatabaseManager();
-        $producten = $database->query("SELECT * FROM producten inner join product_categorieen where product_id = producten.id and categorie_id  = ? and naam like ? ORDER BY naam ASC", [$categorie_id, "%".$filter."%"] )->get();
-        $database->close();
-
-        return $producten;
-    }
-
-    function get_overige_producten()
-    {
-        if (is_array($_GET) && !empty($_GET))
-            $filter = filter_input(INPUT_GET,'filter', FILTER_SANITIZE_SPECIAL_CHARS );
-
-        $database = new DatabaseManager();
-        $producten = $database->query("SELECT * FROM producten WHERE naam like ? and NOT EXISTS ( SELECT * FROM product_categorieen where product_categorieen.product_id = producten.id) ORDER BY naam ASC " , ["%".$filter."%"])->get();
-        $database->close();
-
-        return $producten;
-    }
 
 ?>
--- public/beheer/productbeheer.php
+++ public/beheer/productbeheer.php
@@ -6,13 +6,70 @@
 
 <?php include __DIR__ . '/../../Application/Http/beheer/productbeheer.php'; ?> <!--Verander example.php naar jouw gewenste file-->
 <?php
+     // sessie
      $session = \application\SessionManager::getInstance();
+
+
+     //get and post
      $filter ="";
+     $product_select[0] = "";
+     $product_select[1] = "";
      if (is_array($_GET) && !empty($_GET))
      {
          $filter = filter_input(INPUT_GET, 'filter', FILTER_SANITIZE_SPECIAL_CHARS);
          $filter = trim($filter);
+
+         if(isset($_GET['Product'])) {
+             $product_select = filter_input(INPUT_GET, 'Product', FILTER_SANITIZE_SPECIAL_CHARS);
+             $product_select = preg_split("/ /", $product_select);
+         }
      }
+
+      //database
+      use \application\DatabaseManager;
+      $database = new DatabaseManager();
+        // get categorien
+      $categorieen = $database->query("SELECT * FROM categorieen ORDER BY id ASC")->get();
+        // filter products in categories
+      $producten_categorie = [];
+      foreach ($categorieen as $categorie)
+      {
+          $categorie_id = $categorie['id'];
+          $producten_categorie[$categorie['naam']] = $database->query("SELECT * FROM producten inner join product_categorieen where product_id = producten.id and categorie_id  = ? and naam like ? ORDER BY naam ASC", [$categorie_id, "%".$filter."%"] )->get();
+      }
+        //filter prodcut with no categorie
+      $producten_categorie['overig'] = $database->query("SELECT * FROM producten WHERE naam like ? and NOT EXISTS ( SELECT * FROM product_categorieen where product_categorieen.product_id = producten.id) ORDER BY naam ASC " , ["%".$filter."%"])->get();
+      //close database;
+      $database->close();
+
+      //check selected or filter active
+      foreach  ($producten_categorie as $key => &$item)
+      {
+          //button pressed
+          foreach($item as $_key => $value) {
+              $item[$_key]['show'] = $filter != "" ? "" : "collapse";
+              if ($value['id'] == $product_select[0] && $key == $product_select[1]) {
+                  $item[$_key]['active'] = "Active";
+                  $item['show'] = "";
+              } else
+                  $item[$_key]['active'] = "";
+
+
+          }
+          //filter on categorie
+          $item['show'] = ($filter != "" && count($item) > 0) || array_key_exists("show", $item) ? "" : "collapsed";
+          //if categorie is shown, make all buttons show up
+          foreach($item as $_key => $value)
+          {
+              if( $item['show'] = "")
+              {
+                  $item[$_key]['show'] = "";
+              }
+
+
+          }
+
+      }
 
 ?>
 
@@ -122,6 +179,11 @@
                      </div>
                   </div>
                      <?php include __DIR__ . "/../../Application/Http/beheer/productbeheer_items.php" ?>
+                     <div class="accordion" id="accordionPanelsStayOpenExample">
+
+                         <?php echo arcordion_constructer($producten_categorie) ?>
+
+                     </div>
                  </form>
 
              </aside>
