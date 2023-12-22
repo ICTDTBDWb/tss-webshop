3fa9249e7250ac0192af058c9445f9dfd30beed2
Justin-vdm <126683510+Justin-vdm@users.noreply.github.com>
2023-12-22 08:51:58 +0100

    Login Pagina

--- Resources/views/account/uitloggen.php
+++ Resources/views/account/uitloggen.php
@@ -1,22 +1,4 @@
 <?php
-require_once('/var/www/tss-webshop/Application/Session.php');
 
-
-
-function logout() {
-    // Start de sessie
-    session_start();
-
-
-    // Authenticatie uit de sessie halen
-    unset($_SESSION['auth']['logged_in'],$_SESSION['auth']['user_id']);
-
-    // Omleiden naar de inlogpagina
-    header("Location:/");
-    exit;
-}
-
-
-logout();
-
-?>
+$auth = Auth::getInstance();
+$auth->logout();
