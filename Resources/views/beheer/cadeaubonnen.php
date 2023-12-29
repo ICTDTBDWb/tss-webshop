a423cafde492c41b46ca4a344fb52695cf27f466
jtjscholten <jtjscholten@gmail.com>
2023-12-29 17:31:26 +0100

    auth

--- Resources/views/beheer/cadeaubonnen.php
+++ Resources/views/beheer/cadeaubonnen.php
@@ -1,14 +1,10 @@
 <?php
-$auth->protectAdminPage([Auth::WEBREDACTEUR_ROLE]);
+//$auth->protectAdminPage([Auth::WEBREDACTEUR_ROLE]);
 include basePath('Application/Http/beheer/services.php');
 include basePath("Application/Http/beheer/menu.php");
 
-<<<<<<< HEAD
 //$auth->protectAdminPage([Auth::ADMIN_ROLE]);
 //$current_page = basename($_SERVER['PHP_SELF']);
-=======
-$current_page = basename($_SERVER['PHP_SELF']);
->>>>>>> master
 
 // Verwerk het toevoegen van een cadeaubon
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['toevoegen'])) {
