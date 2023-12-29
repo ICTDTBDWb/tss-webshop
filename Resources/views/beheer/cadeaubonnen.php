1cb4c4dbe1af7191e48611d717683707f87e27d4
Justin-vdm <126683510+Justin-vdm@users.noreply.github.com>
2023-12-29 16:34:08 +0100

    Fix

--- Resources/views/beheer/cadeaubonnen.php
+++ Resources/views/beheer/cadeaubonnen.php
@@ -2,7 +2,6 @@
 include basePath('Application/Http/beheer/services.php');
 include basePath("Application/Http/beheer/menu.php");
 
-$auth->protectAdminPage([Auth::ADMIN_ROLE]);
 $current_page = basename($_SERVER['PHP_SELF']);
 
 // Verwerk het toevoegen van een cadeaubon
