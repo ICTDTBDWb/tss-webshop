93e476051eb36faac6050462eb1a223e94801edd
jtjscholten <jtjscholten@gmail.com>
2024-01-02 13:12:11 +0100

    Functie om input te valideren en ongewenste tekens aan te passen

--- Resources/views/beheer/cadeaubonnen.php
+++ Resources/views/beheer/cadeaubonnen.php
@@ -4,44 +4,49 @@ include basePath('Application/Http/beheer/services.php');
 include basePath("Application/Http/beheer/menu.php");
 $auth->protectAdminPage([Auth::WEBREDACTEUR_ROLE]);
 
+
+
 // Verwerk het toevoegen van een cadeaubon
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['toevoegen'])) {
-    $code = $_POST['code'];
-    $pin = $_POST['pin'];
-    $bedrag = $_POST['bedrag'];
+    $code = valideerInput($_POST['code'], 25);
+    $pin = valideerInput($_POST['pin'], 11);
+    $bedrag = valideerInput($_POST['bedrag'], 12, true);
 
-    // Controleer of de cadeaubon al bestaat
-    if (!cadeaubonBestaat($code)) {
-        // Voeg de cadeaubon toe als deze nog niet bestaat
-        queryVoegCadeaubonToe($code, $pin, $bedrag);
-        echo "<script>alert('Cadeaubon succesvol toegevoegd.');</script>";
+    if (!isValideLengte($code, 25) || !isValideLengte($pin, 11) || !isValideBedrag($bedrag)) {
+        echo "<script>alert('Fout: Onjuiste invoer.');</script>";
     } else {
-        // Toon een foutmelding als de cadeaubon al bestaat
-        echo "<script>alert('Fout: Een cadeaubon met deze code bestaat al.');</script>";
+        $bedrag = number_format((float)$bedrag, 2, '.', '');
+        if (!cadeaubonBestaat($code)) {
+            queryVoegCadeaubonToe($code, $pin, $bedrag);
+            echo "<script>alert('Cadeaubon succesvol toegevoegd.');</script>";
+        } else {
+            echo "<script>alert('Fout: Een cadeaubon met deze code bestaat al.');</script>";
+        }
     }
 }
-
 
 // Verwerk het wijzigen van een cadeaubon
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['wijzigen'])) {
     $cadeaubonId = $_POST['wijzig_cadeaubon_id'];
-    $nieuweCode = $_POST['nieuwe_code'];
-    $nieuwBedrag = $_POST['nieuw_bedrag'];
-// Controleer of de cadeaubon al bestaat
-if (!cadeaubonBestaat($nieuweCode)) {
-    // Wijzig de cadeaubon toe als deze nog niet bestaat
-    queryWijzigCadeaubon($cadeaubonId, $nieuweCode, $nieuwBedrag);
-    echo "<script>alert('Cadeaubon succesvol gewijzigd.');</script>";
+    $nieuweCode = valideerInput($_POST['nieuwe_code'], 25);
+    $nieuwBedrag = valideerInput($_POST['nieuw_bedrag'], 10, true);
+
+    if (!isValideLengte($nieuweCode, 25) || !isValideBedrag($nieuwBedrag)) {
+        echo "<script>alert('Fout: Onjuiste invoer.');</script>";
     } else {
-        // Toon een foutmelding als de cadeaubon al bestaat
-        echo "<script>alert('Fout: Een cadeaubon met deze code bestaat al.');</script>";
+        $nieuwBedrag = number_format((float)$nieuwBedrag, 2, '.', '');
+        if (!cadeaubonBestaat($nieuweCode)) {
+            queryWijzigCadeaubon($cadeaubonId, $nieuweCode, $nieuwBedrag);
+            echo "<script>alert('Cadeaubon succesvol gewijzigd.');</script>";
+        } else {
+            echo "<script>alert('Fout: Een cadeaubon met deze code bestaat al.');</script>";
+        }
     }
 }
 
-// Verwerk het verwijderen van een cadeaubon
+// Verwerkt  het verwijderen van een cadeaubon
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['verwijderen'])) {
     $cadeaubonId = $_POST['cadeaubon_id'];
-
     queryVerwijderCadeaubon($cadeaubonId);
 }
 
@@ -66,7 +71,7 @@ $cadeaubonnen = queryHaalCadeaubonnenOp();
             </div>
             <div class="mb-3">
                 <label for="bedrag" class="form-label">Bedrag:</label>
-                <input type="number" class="form-control" id="bedrag" name="bedrag" required>
+                <input type="number" class="form-control" id="bedrag" name="bedrag" step="0.01" required>
             </div>
             <button type="submit" name="toevoegen" class="btn btn-primary">Voeg Toe</button>
         </form>
@@ -91,11 +96,11 @@ $cadeaubonnen = queryHaalCadeaubonnenOp();
             </div>
             <div class="mb-3">
                 <label for="nieuwe_code" class="form-label">Nieuwe Code:</label>
-                <input type="text" class="form-control" id="nieuwe_code" name="nieuwe_code">
+                <input type="text" class="form-control" id="nieuwe_code" name="nieuwe_code" required>
             </div>
             <div class="mb-3">
                 <label for="nieuw_bedrag" class="form-label">Nieuw Bedrag:</label>
-                <input type="number" class="form-control" id="nieuw_bedrag" name="nieuw_bedrag">
+                <input type="number" class="form-control" id="nieuw_bedrag" name="nieuw_bedrag" step="0.01" required>
             </div>
             <button type="submit" name="wijzigen" class="btn btn-warning">Wijzig</button>
         </form>
