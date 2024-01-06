2e3bd74a52adac10d7bd8201de95f6c45256b21a
juulius <julianvisscher@hotmail.com>
2024-01-06 18:40:06 +0100

    kleine fix

--- Resources/views/beheer/productbeheer.php
+++ Resources/views/beheer/productbeheer.php
@@ -729,11 +729,11 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title">Product Verwijdern</h5>
+                            <h5 class="modal-title">Product Verwijderen</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Sluiten"></button>
                         </div>
                         <div class="modal-body">
-                            <p class='text-center fw-bold'> weet u zeker dat u product <?php $product_naam ?> wilt verwijderen? </p>
+                            <p class='text-center fw-bold'> weet u zeker dat u product <?php echo $product_naam ?> wilt verwijderen? </p>
                         </div>
                         <div class="modal-footer">
                             <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Annuleren</button>
